48e084c0a0fb4a8c59b5b09e8c673087398f651d
Coyls <loic.grecofr@gmail.com>
2021-03-15 20:25:36 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const port = process.env.PORT || 3000;
+
+// -- ExpressJS -- //
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.use(express.static('public'))
+
+http.listen(port, () => {
+    console.log(`http://localhost:${port}/`);
+});
+
+
