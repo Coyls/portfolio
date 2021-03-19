4dd3f9109e05a757d587d1989f59cf7f4c806c38
Coyls <76538870+Coyls@users.noreply.github.com>
2021-03-19 10:41:30 +0100

    test : axios client side

--- public/app.js
+++ public/app.js
@@ -1,1 +1,17 @@
-console.log("Hello world")
+const burgerMenu = document.querySelector("#burger-menu")
+const links = document.querySelector("#links")
+
+burgerMenu.addEventListener("mouseenter", (e) => {
+    links.classList.remove("displayNone")
+})
+
+burgerMenu.addEventListener("mouseleave", (e) => {
+    links.classList.add("displayNone")
+})
+
+axios.request("https://portfoliob1.herokuapp.com/getAPiKey").then( (response) => {
+    console.log(response.data, "api");
+
+}).catch(function (error) {
+    console.error(error);
+});
