1ac4d2d2783f55b7920e4b0ca8d800a7af140a58
Coyls <76538870+Coyls@users.noreply.github.com>
2021-03-19 17:29:40 +0100

    add : script test api

--- public/app.js
+++ public/app.js
@@ -15,3 +15,26 @@ axios.get("https://portfoliob1.herokuapp.com/getAPiKey").then(response => {
 }).catch(function (error) {
     console.error(error);
 });
+
+// ----------------------------------------------------------- test api --------------------------------------------
+// Set the Access Token
+var accessToken = ';
+
+// Call Dribble v2 API
+$.ajax({
+    url: 'https://api.dribbble.com/v2/user/shots?access_token='+accessToken,
+    dataType: 'json',
+    type: 'GET',
+    success: function(data) {
+        if (data.length > 0) {
+            $.each(data.reverse(), function(i, val) {
+                $('#shots').prepend(
+                    '<a class="shot" target="_blank" href="'+ val.html_url +'" title="' + val.title + '"><div class="title">' + val.title + '</div><img src="'+ val.images.hidpi +'"/></a>'
+                )
+            })
+        }
+        else {
+            $('#shots').append('<p>No shots yet!</p>');
+        }
+    }
+});
