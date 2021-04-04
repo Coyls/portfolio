28d8346a4edba7718673f00fd4682d3b47db8a68
Coyls <loic.grecofr@gmail.com>
2021-04-04 12:15:45 +0200

    fix : No Access-Control-Allow-Origin

--- index.js
+++ index.js
@@ -18,11 +18,33 @@ app.use(express.static('public'))
 http.listen(port, () => {
     console.log(`http://localhost:${port}/`);
 });
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 // -------------- //
+
+
 axios.get(`https://api.dribbble.com/v2/user/shots?access_token=${process.env.API_KEY_LOIC}`)
     .then(response => {
         // console.log(response);
         app.get('/getProject', (req,res) => {
+            res.header('Access-Control-Allow-Origin')
             res.send(response.data)
         })
 
