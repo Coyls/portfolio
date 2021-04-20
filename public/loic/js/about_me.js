52fda2879f78af97947fbabbb63bbbc7d08743a6
Coyls <loic.grecofr@gmail.com>
2021-04-20 03:58:03 +0200

    add : split_text.js

--- public/loic/js/about_me.js
+++ public/loic/js/about_me.js
@@ -1,21 +1,53 @@
 import { headerFooterCreation } from './header_footer.js'
-import { requestServer } from './request.js'
+import { requestServer} from './request.js'
+import { splitElements} from './split_texts.js'
 
 headerFooterCreation()
 
-requestServer('getUserChloe').then(response => {
+requestServer('getUserLoic').then(response => {
 
-console.log(response.data)
+    console.log(response.data)
 
-let bio = response.data.bio
+    let bio = response.data.bio
 
-const bioParagraph = document.getElementById('bio-paragraph')
 
-bioParagraph.innerHTML = bio
+    
 
-/* let formation = "Le Bachelor Designer Web et Mobile en 3 ans aux Gobelins m’a fait acquérir de multiples capacités tel que : le motion design, la déclinaison de design, le montage vidéo, l’UX et l’UI, la création de visuels graphiques (charte graphique, logos, icônes...), la culture (design, jeux-vidéos, générale, ...), ...etc"
-const formationParagraph = document.getElementById('formation-paragraph')
-formationParagraph.innerHTML = formation */
+    
+    const aboutMe = splitElements(bio, '$')
+    const formation = splitElements(bio, '§')
+    const experiencesContainer = splitElements(bio, 'µ')
+    const experiences = splitElements(experiencesContainer, '*')
+
+    const bioParagraph = document.getElementById('bio-paragraph')
+    const formationParagraph = document.getElementById('formation-paragraph')
+    const experiencesParagraph = document.getElementById('experience-paragraph')
+
+    bioParagraph.innerHTML = aboutMe
+    formationParagraph.innerHTML = formation
+
+    experiences.forEach(exp => {
+        experiencesParagraph.innerHTML += `<p>${exp}</p>`
+    })
+
+
+
+    console.log(aboutMe)
+    console.log(formation)
+    console.log(experiences)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 })
---
+++ public/loic/js/split_texts.js
@@ -0,0 +1,37 @@
+export const splitElements = (arrayToSplit, character) => {
+    let arraySplit = arrayToSplit.split('')
+
+    let mark = false
+    let idStart = 0
+    let idEnd = 0
+
+    let result = arraySplit
+    let arrayToReturn = []
+    let i = 0
+
+    arraySplit.forEach((char, id) => {
+        if (char === character && !mark) {
+            idStart = id + 1
+            mark = true
+        } else if (char === character && mark) {
+            idEnd = id
+            mark = false
+
+            if (character === '*') {
+                let t = result.slice(idStart, idEnd)
+                arrayToReturn[i] = t.join('')
+                i++
+            }
+        }
+    })
+
+    if (character === '*') {
+        return arrayToReturn
+    } else {
+        result = result.slice(idStart, idEnd)
+        result = result.join('')
+        return result
+    }
+
+
+}
