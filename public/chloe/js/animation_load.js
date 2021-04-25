13b45a07b79a976ed394d469a06f680c011025bd
Coyls <loic.grecofr@gmail.com>
2021-04-25 04:20:34 +0200

    fix : add muted to fix no run animation

--- public/chloe/js/animation_load.js
+++ public/chloe/js/animation_load.js
@@ -1,8 +1,7 @@
-const locationIndex = document.location.origin
-
 let video = document.getElementById("animation-load")
+
 video.onended = () => {
 
-    location.replace(`${locationIndex}/chloe/index.html`)
+    location.replace(`${document.location.origin}/chloe/index.html`)
 
 };
