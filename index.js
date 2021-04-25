a8494c15dd44b030bd4cdd2617a9c23f7ebc8c6a
Coyls <loic.grecofr@gmail.com>
2021-04-25 17:31:40 +0200

    OK : Cors debug

--- index.js
+++ index.js
@@ -40,23 +40,6 @@ http.listen(port, () => {
     console.log(`http://localhost:${port}/`);
 });
 
-/* app.use(function (req, res, next) {
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', '*');
-    // Request headers you wish to allow
-
-    // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    // Pass to next layer of middleware
-    next();
-}); */
 // -------------------------------------------------------------------------------------------------- //
 
 const requestAPiDribbble = async (key, link, path) => {
