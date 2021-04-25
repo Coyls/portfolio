4652ca10c6d8354b58e43633ac07a103abe7a9ea
Coyls <loic.grecofr@gmail.com>
2021-04-25 17:14:15 +0200

    fix : pls

--- index.js
+++ index.js
@@ -44,11 +44,9 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', '*');
     // Request headers you wish to allow
 
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
-    // res.setheader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
