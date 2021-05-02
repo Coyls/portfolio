4f52cdce96beda79ef4beb15d96b828ac8c865f3
Coyls <76538870+Coyls@users.noreply.github.com>
2021-05-02 14:28:37 +0200

    fix home project hidden buy size

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.static(__dirname + '/public'));
 
 // -- Root -- //
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/jeanne/index.html');
+    res.sendFile(__dirname + '/public/loic/index.html');
 });
 
 // -- link -- //
