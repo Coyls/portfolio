32f3bf4b88dc5fc84ad723e98f55be9a47308ee2
Coyls <loic.grecofr@gmail.com>
2021-05-04 17:31:39 +0200

    update do screen

--- public/globals_assets/js/request.js
+++ public/globals_assets/js/request.js
@@ -1,11 +1,9 @@
+
 export const requestServer = async (path) => {
+
     const locationApi = document.location.origin
 
-    console.log(`${locationApi}${path}`);
-
-
     const response = await axios.get(`${locationApi}/${path}`)
-
 
     try {
         return response
