0c0feed032806a6769b8a4b3053beb62d94dd016
Coyls <loic.grecofr@gmail.com>
2021-05-06 19:39:37 +0200

    VERSION STABLE POUR SOUTENANCE

--- public/jeanne/js/header_footer.js
+++ public/jeanne/js/header_footer.js
@@ -37,9 +37,4 @@ export const headerFooterCreation = () => {
     <a id="msg" href="mailto:jeanne.vdp@outlook.fr"></a>`;
 
   scrollTop();
-
-  headerAnimationLogo(
-    ".logo",
-    `${location}/chloe/animations/animation_logo_chloe.json`
-  );
 };
